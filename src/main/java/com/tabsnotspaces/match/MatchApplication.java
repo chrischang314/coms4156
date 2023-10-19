02dcc4a9ee59a389088cf54bb5993ce1f874e1ac
sm4410 <sm4410@columbia.edu>
2023-10-19 11:08:41 -0400

    Add API documentation and README file

--- src/main/java/com/tabsnotspaces/match/MatchApplication.java
+++ src/main/java/com/tabsnotspaces/match/MatchApplication.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class represents the entry point of the Match Application.
+ */
 @SpringBootApplication
 public class MatchApplication {
 	
@@ -22,6 +25,11 @@ public class MatchApplication {
 	@Autowired
 	AppointmentRepository appointmentRepository;
 
+	/**
+	 * The main method to start the Spring Boot application.
+	 *
+	 * @param args Command-line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MatchApplication.class, args);
 	}
